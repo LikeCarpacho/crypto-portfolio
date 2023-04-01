d20beb2958137d01dc46d0cd5c91ab4a94dc3148
Joel <joel@Joels-MacBook-Pro.local>
2023-04-01 17:59:05 -0500

    server fix

--- portfolio-tracker/server.js
+++ portfolio-tracker/server.js
@@ -4,10 +4,19 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 4200;
 
-app.use(express.static('./dist/your-angular-app'));
+// Set correct MIME types for static files
+app.use(express.static('./dist/portfolio-tracker', {
+  setHeaders: (res, path) => {
+    if (path.endsWith('.js')) {
+      res.setHeader('Content-Type', 'application/javascript');
+    } else if (path.endsWith('.css')) {
+      res.setHeader('Content-Type', 'text/css');
+    }
+  }
+}));
 
 app.get('/*', (req, res) => {
-  res.sendFile('index.html', {root: 'dist/index.html/'});
+  res.sendFile('index.html', {root: 'dist/your-angular-app/'});
 });
 
 app.listen(PORT, () => {
