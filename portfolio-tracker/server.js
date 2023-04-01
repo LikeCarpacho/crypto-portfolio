14912c7b508e9151f5e25267521eff4749d8f92b
Joel <joel@Joels-MacBook-Pro.local>
2023-04-01 18:02:29 -0500

    server fix

--- portfolio-tracker/server.js
+++ portfolio-tracker/server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = 0;
+const PORT = process.env.PORT || 4200;
+
 
 // Set correct MIME types for static files
 app.use(express.static('./dist/portfolio-tracker', {
