a741894ee8c9966d9b83855dc50c904b2413c933
Joel <joel@Joels-MacBook-Pro.local>
2023-04-02 12:22:17 -0500

    table start

---
+++ portfolio-tracker/src/app/crypto-table/crypto-table.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crypto-table',
+  templateUrl: './crypto-table.component.html',
+  styleUrls: ['./crypto-table.component.scss']
+})
+export class CryptoTableComponent {
+
+}
