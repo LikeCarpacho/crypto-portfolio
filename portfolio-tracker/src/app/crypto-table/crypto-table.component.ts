cf41263353bc9b9ebcddda09cd56e81fa04027c3
Joel <joel@Joels-MacBook-Pro.local>
2023-04-02 22:27:13 -0500

    Filter the table  now

--- portfolio-tracker/src/app/crypto-table/crypto-table.component.ts
+++ portfolio-tracker/src/app/crypto-table/crypto-table.component.ts
@@ -16,4 +16,31 @@ export class CryptoTableComponent {
   deleteEntry(symbol: string) {
     this.entryDeleted.emit(symbol);
   }
+  currentSort = {
+    column: '',
+    order: 'asc'
+  };
+
+  sortTable(column: string) {
+    if (this.currentSort.column === column) {
+      this.currentSort.order = this.currentSort.order === 'asc' ? 'desc' : 'asc';
+    } else {
+      this.currentSort.column = column;
+      this.currentSort.order = 'asc';
+    }
+
+    this.portfolio.sort((a, b) => {
+      let comparison = 0;
+
+      if (column === 'totalValue') {
+        const aValue = a.amount * a.usdPrice;
+        const bValue = b.amount * b.usdPrice;
+        comparison = aValue > bValue ? 1 : aValue < bValue ? -1 : 0;
+      } else {
+        comparison = (a as any)[column] > (b as any)[column] ? 1 : (a as any)[column] < (b as any)[column] ? -1 : 0;
+      }
+
+      return this.currentSort.order === 'asc' ? comparison : -comparison;
+    });
+  }
 }
